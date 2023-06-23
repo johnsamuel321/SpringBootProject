435462aec75ab9eacba3e69d7f75e00a9f527552
johnsamuel <YOU@EXAMPLE.COM>
2023-06-23 13:41:01 +0530

    second commit

--- src/main/java/com/pack/SpringGit/Controller/HelloController.java
+++ src/main/java/com/pack/SpringGit/Controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
 	public String getMessage() {
-		return "Hello World";
+		return"From controller";
 		
 	}
 }
