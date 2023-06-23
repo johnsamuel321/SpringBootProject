413f9929f98b2787f4b6fff19d35643825832730
johnsamuel <YOU@EXAMPLE.COM>
2023-06-23 11:39:25 +0530

    first commit

---
+++ src/test/java/com/pack/SpringGit/SpringGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pack.SpringGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
